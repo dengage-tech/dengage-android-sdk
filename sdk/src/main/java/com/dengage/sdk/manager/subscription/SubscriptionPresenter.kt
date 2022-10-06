bb0e99ade5481b84066ac287a412bf87b84c8fb6
hasnain <hasnain.tanvir@dengage.com>
2022-10-07 03:02:56 +0500

    Subsciption glith removed

--- sdk/src/main/java/com/dengage/sdk/manager/subscription/SubscriptionPresenter.kt
+++ sdk/src/main/java/com/dengage/sdk/manager/subscription/SubscriptionPresenter.kt
@@ -7,40 +7,50 @@ import com.dengage.sdk.domain.subscription.usecase.SendSubscription
 import com.dengage.sdk.manager.base.BaseAbstractPresenter
 import com.dengage.sdk.util.Constants
 import com.dengage.sdk.util.DengageLogger
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SubscriptionPresenter : BaseAbstractPresenter<SubscriptionContract.View>(),
     SubscriptionContract.Presenter {
 
     private val sendSubscription by lazy { SendSubscription() }
+    private val scope = CoroutineScope(Dispatchers.IO)
 
     private var sendSubscriptionTryCount = 0
 
     override fun sendSubscription(subscription: Subscription) {
-        Handler(Looper.getMainLooper()).postDelayed({
+        scope.launch {
+            delay(4000)
             if (Constants.sendSubscription) {
-                DengageLogger.verbose("sub method is called")
-                sendSubscriptionTryCount++
-                sendSubscription(this) {
-                    onResponse = {
-                        view { subscriptionSent() }
-                    }
-                    onError = {
-                        // try to send it for 5 times
-                        if (sendSubscriptionTryCount < 5) {
-                            sendSubscription(
-                                subscription = subscription
-                            )
-                        } else {
-                            sendSubscriptionTryCount = 0
-                        }
-                    }
-                    params = SendSubscription.Params(
-                        subscription = subscription
-                    )
-                }
+                callSubscriptionApi(subscription)
             }
-        }, 4000)
 
+        }
     }
 
+
+    private fun callSubscriptionApi(subscription: Subscription) {
+        DengageLogger.verbose("sub method is called")
+        sendSubscriptionTryCount++
+        sendSubscription(this) {
+            onResponse = {
+                view { subscriptionSent() }
+            }
+            onError = {
+                // try to send it for 5 times
+                if (sendSubscriptionTryCount < 5) {
+                    sendSubscription(
+                        subscription = subscription
+                    )
+                } else {
+                    sendSubscriptionTryCount = 0
+                }
+            }
+            params = SendSubscription.Params(
+                subscription = subscription
+            )
+        }
+    }
 }
