cb92714f61b03e336caa0e70419152b3298bfd2b
hasnain <hasnain.tanvir@dengage.com>
2023-05-28 14:59:40 +0500

    Inbox Messages proguard issue resolved

--- sdk/src/main/java/com/dengage/sdk/domain/push/model/CarouselItem.kt
+++ sdk/src/main/java/com/dengage/sdk/domain/push/model/CarouselItem.kt
@@ -4,11 +4,11 @@ import com.google.gson.annotations.SerializedName
 import java.io.Serializable
 
 data class CarouselItem(
-    @SerializedName("id") val id: String = "",
-    @SerializedName("title") val title: String = "",
-    @SerializedName("desc") val description: String = "",
-    @SerializedName("mediaUrl") val mediaUrl: String = "",
-    @SerializedName("targetUrl") val targetUrl: String = "",
+    @SerializedName("id") var id: String = "",
+    @SerializedName("title") var title: String = "",
+    @SerializedName("desc") var description: String = "",
+    @SerializedName("mediaUrl") var mediaUrl: String = "",
+    @SerializedName("targetUrl") var targetUrl: String = "",
     @SerializedName("type") var type: String = "",
     @SerializedName("mediaFileLocation") var mediaFileLocation: String? = null,
     @SerializedName("mediaFileName") var mediaFileName: String? = null
--- sdk/src/main/java/com/dengage/sdk/util/DengageUtils.kt
+++ sdk/src/main/java/com/dengage/sdk/util/DengageUtils.kt
@@ -55,7 +55,7 @@ object DengageUtils {
     }
 
     fun getSdkVersion(): String {
-        return "6.0.31.4"
+        return "6.0.32.4"
     }
 
     fun getUserAgent(context: Context): String {
